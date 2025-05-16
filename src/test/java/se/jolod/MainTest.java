0ebc0778cc3af1c4e3a9ccfa079ea7c4cb54f5ca
Johan Lodin <jolod@users.noreply.github.com>
2025-05-16 12:29:54 +0700

    Initial setup.

---
+++ src/test/java/se/jolod/MainTest.java
@@ -0,0 +1,12 @@
+package se.jolod;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class MainTest {
+    @Test
+    @DisplayName("WIP: Testing the setup.")
+    void aPassingTest() {
+        assertTrue(true,"True is true.");
+    }
+}
